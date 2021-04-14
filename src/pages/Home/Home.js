e72f5d6e795a7b8f27b80058f19f704805baf1fd
Tomas Olsen <tomas.kolsen@gmail.com>
2021-04-14 15:14:59 +0200

    Add navbar and footer template

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,9 @@ import { Container, Typography } from '@material-ui/core'
 import React from 'react'
 
 export default function Home() {
-    return (
-        <div>
-            <Container>
-                <Typography variant="h1">Oboe</Typography>
-            </Container>
-        </div>
-    )
+  return (
+    <Container>
+      <Typography variant="h1">Oboe</Typography>
+    </Container>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 import Home from './Home/Home';
 
+import styled from 'styled-components';
+
+const Body = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
+
+const StyledFooter = styled.div`
+  justify-self: flex-end;
+  background-color: red;
+`
+
+const StyledHeader = styled.div`
+  justify-self: flex-start;
+`;
+
+const StyledMainContent = styled.div`
+  flex-grow: 1;
+`;
+
 export default function Pages() {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={Home} />
-            </Switch>
-        </div>
-    )
+  return (
+    <Body>
+      <StyledHeader>
+        <Navbar />
+      </StyledHeader>
+
+      <StyledMainContent>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </StyledMainContent>
+
+      <StyledFooter>
+        <Footer />
+      </StyledFooter>
+    </Body>
+  )
 };
