b8e913e298142d0f7615af93b6978093faa6d8be
simen wiik <simenwiik@hotmail.com>
2021-04-25 00:08:41 +0200

    Review card styling

--- src/components/ReviewCard/ReviewCardStyled.js
+++ src/components/ReviewCard/ReviewCardStyled.js
@@ -5,16 +5,24 @@ import styled from 'styled-components';
  * The card itself, with background color and padding
  */
 export const StyledReviewCard = styled.div`
-  position: relative;
-  width: 300px;
-  height: 400px;
+  ${({ theme }) => `
+  ${theme.breakpoints.down("xs")} {
+    width: 100%;
+    min-height: 35vh;
+  }
+  ${theme.breakpoints.up("sm")} {
+    width: 300px;
+    min-height: 400px;
+  }
+    position: relative;
+  `}
 `;
 
 /**
  * Applies to the text/button content inside the card
  */
 export const StyledFrontFace = styled(animated.div)`
-  background-color: ${props => props.color};
+  background: linear-gradient(0, rgba(2,0,36,1) 0%, ${props => props.color} 100%);
   position: absolute;
   top: 0;
   left: 0;
