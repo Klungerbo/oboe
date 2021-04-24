a4afb4ce35a14e9feb9960bc29171ecfc317c06f
simen wiik <simenwiik@hotmail.com>
2021-04-25 00:10:08 +0200

    Changed column layout of review page

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   Grid, Box, Hidden,
-  Dialog
+  Container
 } from '@material-ui/core'
 import SendIcon from '@material-ui/icons/Send';
 import ReviewHelp from '../../components/ReviewHelp/ReviewHelp'
@@ -17,24 +17,27 @@ export default function Review() {
   const [reviewStats, setReviewStats] = React.useState({ correct: 0, incorrect: 0, cardsLeft: 0 });
 
   return (
-    <Grid container>
-      <Hidden smDown>
-        <Grid item container md={2}>
-          <Box display="flex" alignSelf="flex-end">
-            <ReviewHelp></ReviewHelp>
-          </Box>
-        </Grid>
-      </Hidden>
+    <Container>
+      <Grid container style={{height: "100%"}}>
+        <Hidden smDown>
+          <Grid item container md={4}>
+            <Box display="flex" pl={2} alignSelf="flex-end">
+              <ReviewHelp></ReviewHelp>
+            </Box>
+          </Grid>
+        </Hidden>
+        <Grid container item direction="column" alignItems="center" justify="center" xs={12} md={4}>
           <Box width={280}>
-        <ReviewCard deckid={id} cardColor={color} setCardColor={setColor} reviewStats={reviewStats} setReviewStats={setReviewStats} />
+            <ReviewCard deckid={id} cardColor={color} setCardColor={setColor} reviewStats={reviewStats} setReviewStats={setReviewStats} />
             <ReviewStats reviewStats={reviewStats} />
           </Box>
-        <ExitToDecksButton component={Link} to="/" exitbuttoncolor={color} variant="contained" endIcon={<SendIcon />}>Wrap up session</ExitToDecksButton>
-      </Grid>
-      <Hidden>
-        <Grid item md={2}>
+          <ExitToDecksButton component={Link} to="/" exitbuttoncolor={color} variant="contained" endIcon={<SendIcon />}>Wrap up session</ExitToDecksButton>
         </Grid>
-      </Hidden>
-    </Grid>
+        <Hidden>
+          <Grid item md={4}>
+          </Grid>
+        </Hidden>
+      </Grid>
+    </Container>
   )
 }
