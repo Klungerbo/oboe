80acea25b7e2e3f48699fe943bdbcaf43bd0836a
Tomas Olsen <tomas.kolsen@gmail.com>
2021-04-28 03:18:24 +0200

    Better scaling for cards

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,6 +8,11 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import EmailIcon from '@material-ui/icons/Email';
 import styled from 'styled-components';
 
+import { Icon, InlineIcon } from '@iconify/react';
+import soundcloudIcon from '@iconify-icons/zmdi/soundcloud';
+import discordIcon from '@iconify-icons/bi/discord';
+
+
 
 export default function About() {
   return (
@@ -15,7 +20,7 @@ export default function About() {
       <Grid container>
         <Grid item xs={12}>
           <Typography variant="h1">About</Typography>
-          <Box ml={3}>
+          <Box mx={3}>
             <Typography variant="h2">School project</Typography>
             <Typography variant="body1" gutterBottom>
               Oboe resulted from a mandatory activity at NTNU IDATA2301 Webteknologi (2021),
@@ -24,34 +29,36 @@ export default function About() {
           </Box>
         </Grid>
         <Grid item xs={12}>
-          <Box ml={3}>
+          <Box mx={3}>
             <Typography variant="h2">The team</Typography>
             <Typography variant="body1" gutterBottom>
               The team consists of two developers, Tomas Klungerbo Olsen and Simen Nesse Wiik.
             </Typography>
-            <Box display="flex" flexDirection="row" flexWrap="wrap">
-              <Box mb={2} mr={2}>
+            <Grid container spacing={2} style={{maxWidth: "max-content"}}>
+              <Grid item container xs={12} sm={6} >
                 <PersonInfoCard
                   avatar={"/assets/snw_avatar.jpg"}
                   name="Simen Nesse Wiik"
                   github={{link: "https://github.com/revosw", name: "revosw"}}
+                  soundcloud={{link: "https://soundcloud.com/simenwiik", name: "simenwiik"}}
+                  discord={{name: "Revolution#1234"}}
                 />
-              </Box>
-              <Box mb={2}>
+              </Grid>
+              <Grid item container xs={12} sm={6} >
                 <PersonInfoCard
                   avatar={"/assets/tko_avatar.jpg"}
                   name="Tomas Klungerbo Olsen"
                   github={{link: "https://github.com/Klungerbo", name: "Klungerbo"}}
-                  mail={{link: "mailto:tomas@klungerbo.com", name: "tomas@klungerbo.com"}}
+                  discord={{name: "Nopsa#6931"}}
                 />
-              </Box>
-            </Box>
+              </Grid>
+            </Grid>
           </Box>
         </Grid>
         <Grid item xs={12}>
           <Box py={4} />
           <Typography variant="h2">What is Oboe?</Typography>
-          <Box ml={3}>
+          <Box mx={3}>
             <Typography variant="h3">Decks and cards</Typography>
             <Typography variant="body1" gutterBottom>
               With Oboe, you can create, manage, and review decks of flashcards.
@@ -70,71 +77,104 @@ export default function About() {
   );
 }
 
-const PersonInfoCard = ({ name, avatar, github, mail }) => {
+const PersonInfoCard = ({ name, avatar, github, mail, soundcloud, discord }) => {
   return (
     <StyledCard>
       <Box
-        mx={1.5} py={1.5} minHeight="100%"
-        display="flex" flexDirection="column" justifyContent="space-between"
+        style={{height: "100%", flexShrink: 1}} flexBasis={323} p={1.5} 
+        display="flex" flexDirection="column" 
       >
-        <Box flexGrow="1">
+        <Box>
           <Typography gutterBottom style={{ fontSize: "1.5rem", fontFamily: "Bebas Neue" }}>
             {name}
           </Typography>
         </Box>
-        <Box display="flex" direction="row" flexWrap="wrap">
-          <Box flexGrow="1">
+        <StyledMediaContainer flexBasis={100} flexGrow="0" display="flex" direction="row" flexWrap="wrap">
+          <Box flexGrow="1" pb={1.5}>
             {github && github.link && github.name &&
-              <Typography variant="body2" style={{fontSize: "0.85rem"}}>
+              <Typography variant="body2" style={{fontSize: "1rem"}}>
                 <GitHubIcon fontSize="inherit" /> GitHub:
                 <Link style={{ color: "#00D1B9" }} href={github.link}> {github.name}</Link>
               </Typography>
             }
             {mail && mail.link && mail.name &&
-              <Typography variant="body2" style={{fontSize: "0.85rem"}}><EmailIcon fontSize="inherit" /> Mail:
+              <Typography variant="body2" style={{fontSize: "1rem"}}><EmailIcon fontSize="inherit" /> Mail:
                 <Link style={{ color: "#00D1B9" }} href={mail.link}> {mail.name}</Link>
+              </Typography>
+            }
+            {soundcloud?.link && soundcloud?.name &&
+              <Typography variant="body2" style={{fontSize: "1rem"}}><Icon icon={soundcloudIcon} fontSize="inherit" /> Soundcloud:
+                <Link style={{ color: "#00D1B9" }} href={soundcloud.link}> {soundcloud.name}</Link>
+              </Typography>
+            }
+            {discord?.name &&
+              <Typography variant="body2" style={{fontSize: "1rem"}}><Icon icon={discordIcon} fontSize="inherit" /> Discord: {discord.name}
               </Typography>
             }
           </Box>
           <StyledAvatar src={avatar} />
+        </StyledMediaContainer>
+        <Box flexGrow="1"/>
+        <Box display="flex" pt={1}>
+          <StyledLogo src="/assets/ntnu-logo.png" />
         </Box>
-        <Box flexGrow="1" />
-        <Box pt={1}><StyledLogo src="/assets/ntnu-logo.png" /></Box>
       </Box>
     </StyledCard>
   );
 }
 
-const StyledAvatar = styled.img`
+const StyledMediaContainer = styled(Box)`
   ${({ theme }) => `
-    width: 100px;
-    heigh: 100px;
+    flex-direction: row;
 
     ${theme.breakpoints.down('xs')} {
-      width: 75px;
-      heigh: 75px;
+      flex-direction: column;
+    }
+  `}
+`;
+
+const StyledAvatar = styled.img`
+  ${({ theme }) => `
+    ${theme.breakpoints.up('xs')} {
+      width: 25vw;
+      height: 25vw;
+    }
+    ${theme.breakpoints.up('sm')} {
+      width: 11vw;
+      height: 11vw;
+    }
+    ${theme.breakpoints.up('md')} {
+      width: 10vw;
+      height: 10vw;
+    }
+    ${theme.breakpoints.up('lg')} {
+      width: 10vw;
+      height: 10vw;
+    }
+    ${theme.breakpoints.up('xl')} {
+      width: 7vw;
+      height: 7vw;
     }
   `}
 `;
 
 const StyledLogo = styled.img`
   ${({ theme }) => `
-    width: 300px;
+    width: 40%;
+    flex-basis: 400px;
+    flex-shrink: 1;
 
     ${theme.breakpoints.down('xs')} {
-      width: 266px;
     }
   `}
 `;
 
 const StyledCard = styled(Paper)`
   ${({ theme }) => `
-    min-width: 323px;
     height: 100%;
+    max-width: 425px;
 
     ${theme.breakpoints.down('xs')} {
-      width: 290px;
-      height: {290 * 0.618}px;
     }
   `}
 `;
