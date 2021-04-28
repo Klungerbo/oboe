15e4762e05679adb3614564c66b8fce08540ffb7
simen wiik <simenwiik@hotmail.com>
2021-04-28 21:17:17 +0200

    Edit page before testing refactor

--- src/pages/Edit/Edit.js
+++ src/pages/Edit/Edit.js
@@ -1,41 +1,42 @@
 import {
   Container, Typography, Box,
-  TextField, TextareaAutosize, Grid,
-  Paper, Button, useTheme
+  TextField, Grid, Paper,
+  Button
 } from '@material-ui/core'
 import { ColorPalette } from "material-ui-color";
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentDeck } from '../../store/actions/DataActions'
 import flashcards from '../../data/flashcards'
-import colors from '../../data/colors'
+import { colorPalette } from '../../data/colors'
 import { useParams } from 'react-router';
 
+const dispatch = useDispatch();
+const MAX_DECK_DESCRIPTION_LENGTH = 150;
+const MAX_TITLE_LENGTH = 50;
+const MAX_FRONT_LENGTH = 100;
+const MAX_BACK_LENGTH = 100;
+const MAX_CARD_DESCRIPTION_LENGTH = 100;
+const currentDeck = useSelector(state => state.currentDeck);
+
+
 export default function Edit() {
-  const palette = colors
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  .reduce((_color, curr) => {
-    const {name} = _color;
-    const {color} = _color;
-    curr[name] = color;
-    return {..._color, curr};
-  });
-  console.log(palette)
-  const {id} = useParams();
-  const dispatch = useDispatch();
-  const currentDeck = useSelector(state => state.currentDeck);
-
+  const { id } = useParams();
   const [isEditing, setIsEditing] = useState(0);
+  const [deckDescriptionText, setDeckDescriptionText] = useState("");
+  const [deckTitleText, setDeckTitleText] = useState("");
+  const [deckColor, setDeckColor] = useState("");
 
+  const [cardDescriptionText, setCardDescriptionText] = useState("")
+  const [cardFrontText, setCardFrontText] = useState("")
+  const [cardBackText, setCardBackText] = useState("")
+
+  useEffect(() => {
+    const colorCode = colorPalette[deckColor];
+    dispatch(setCurrentDeck({ ...currentDeck, cardColor: colorCode }))
+    console.log(currentDeck)
+  }, [deckColor])
   const editCard = card => {
     setIsEditing(card.id);
   }
@@ -46,18 +47,20 @@ export default function Edit() {
 
   const maybeDisable = card => {
     setTimeout(() => {
-      if (document.activeElement.closest(`.-c-${card.id}`)) {
+      if (document.activeElement.closest(`card${card.id}`)) {
         setIsEditing(card.id)
       }
       else {
         setIsEditing(0)
       }
-    }, 0)
+    }, 1000)
   }
+
+  console.log("running outside")
 
   useEffect(() => {
     dispatch(setCurrentDeck(flashcards))
-
+    console.log("running ")
   }, [])
 
   return (
@@ -67,32 +70,42 @@ export default function Edit() {
         <Grid container spacing={4}>
           <Grid item xs={4}>
             <Box display="flex" flexDirection="column">
-              <TextField variant="outlined" label="Title" />
-              <TextareaAutosize rowsMin={10} />
+              <TextField variant="outlined"
+                label={`Title (max ${MAX_TITLE_LENGTH} characters)`}
+                inputProps={{ maxLength: MAX_TITLE_LENGTH }}
+                onChange={e => setDeckTitleText(e.target.value)} defaultValue={deckTitleText}
+                helperText={`${deckTitleText.length} / ${MAX_TITLE_LENGTH}`} />
+              <Box p={1} />
+              <TextField variant="outlined"
+                inputProps={{ maxLength: MAX_DECK_DESCRIPTION_LENGTH }}
+                onChange={e => setDeckDescriptionText(e.target.value)}
+                label={`Description (max ${MAX_DECK_DESCRIPTION_LENGTH} characters)`}
+                multiline rows={4} defaultValue={deckDescriptionText}
+                helperText={`${deckDescriptionText.length} / ${MAX_DECK_DESCRIPTION_LENGTH}`} />
               <Box display="flex">
                 <Typography>Color code: </Typography>
-                <ColorPalette palette={palette}></ColorPalette>
+                <ColorPalette onSelect={e => setDeckColor(e)} palette={colorPalette}></ColorPalette>
                 <Box p={4} bgcolor=""></Box>
               </Box>
             </Box>
           </Grid>
           <Grid item xs={4}>
             <Box height={1}>
-              <Paper style={{ height: "100%" }}>
+              <Paper style={{ height: "100%", backgroundColor: currentDeck.cardColor }}>
                 <Box display="flex" flexDirection="column" flexDirection="column"
                   alignItems="center" justifyContent="center" style={{ height: "100%" }}>
-                  <Typography variant="h2">Front of current card</Typography>
+                  <Typography variant="h2">{cardFrontText}</Typography>
                 </Box>
               </Paper>
             </Box>
           </Grid>
           <Grid item xs={4}>
             <Box height={1}>
-              <Paper style={{ height: "100%" }}>
+              <Paper style={{ height: "100%", backgroundColor: currentDeck.cardColor }}>
                 <Box p={3} display="flex" flexDirection="column" flexDirection="column"
                   alignItems="center" justifyContent="center" style={{ height: "100%" }}>
-                  <Typography variant="h2">Back of current card</Typography>
-                  <Typography variant="body2">Description of card. Very long description to see if the card can handle it</Typography>
+                  <Typography variant="h2">{cardBackText}</Typography>
+                  <Typography variant="body2">{cardDescriptionText}</Typography>
                 </Box>
               </Paper>
             </Box>
@@ -104,32 +117,32 @@ export default function Edit() {
         <Typography variant="h2">Add card</Typography>
       </Box>
       <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <TextField variant="outlined" label="Front" fullWidth />
+        <Grid item xs={2}>
+          <TextField onChange={e => setCardFrontText(e.target.value)} variant="outlined" label="Front" fullWidth />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField onChange={e => setCardBackText(e.target.value)} variant="outlined" label="Back" fullWidth />
+        </Grid>
+        <Grid item xs={5}>
+          <TextField onChange={e => setCardDescriptionText(e.target.value)} variant="outlined" label="Description" fullWidth />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Back" fullWidth />
-        </Grid>
-        <Grid item xs={3}>
-          <TextField variant="outlined" label="Description" fullWidth />
-        </Grid>
-        <Grid item xs={3}>
-          <Button variant="contained">Add card</Button>
+          <Button color="primary" variant="contained">Add card</Button>
         </Grid>
       </Grid>
       <Box px={5} pb={1} pt={5}>
         <Typography variant="h2">Cards in deck</Typography>
       </Box>
-      {currentDeck.map(card => (
-        <Grid container spacing={3}>
-          <Grid item xs={3}>
-            <TextField className={"-c-" +card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Front" disabled={isEditing !== card.id} fullWidth defaultValue={card.frontside}></TextField>
+      {flashcards.map(card => (
+        <Grid key={card.id} container spacing={3}>
+          <Grid item xs={2}>
+            <TextField className={"card" + card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Front" disabled={isEditing !== card.id} fullWidth defaultValue={card.frontside}></TextField>
           </Grid>
-          <Grid item xs={3}>
-            <TextField className={"-c-" +card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Front" disabled={isEditing !== card.id} fullWidth defaultValue={card.backside}></TextField>
+          <Grid item xs={2}>
+            <TextField inputProps className={"card" + card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Front" disabled={isEditing !== card.id} fullWidth defaultValue={card.backside}></TextField>
           </Grid>
-          <Grid item xs={3}>
-            <TextField className={"-c-" +card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Description" disabled={isEditing !== card.id} fullWidth defaultValue={card.description}></TextField>
+          <Grid item xs={5}>
+            <TextField className={"card" + card.id} variant="outlined" onBlur={() => maybeDisable(card)} aria-label="Description" disabled={isEditing !== card.id} fullWidth defaultValue={card.description}></TextField>
           </Grid>
           <Grid item xs={1}>
             <Button variant="contained" color="primary" onClick={() => editCard(card)}>Edit</Button>
