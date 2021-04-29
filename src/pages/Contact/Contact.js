9c22d39aeed703ae70bdf4a48ba56d1ef7ecff5c
simen wiik <simenwiik@hotmail.com>
2021-04-30 00:14:10 +0200

    Fixed indenting

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,25 +5,25 @@ export default function Contact() {
   return (
     <Container maxWidth="lg">
       <Typography variant="h1">Contact</Typography>
-      <Grid container spacing={2} style={{maxWidth: "max-content"}}>
-              <Grid item container xs={12} sm={12} md={6}>
-                <PersonInfoCard
-                  avatar={"/assets/snw_avatar.jpg"}
-                  name="Simen Nesse Wiik"
-                  github={{link: "https://github.com/revosw", name: "revosw"}}
-                  soundcloud={{link: "https://soundcloud.com/simenwiik", name: "simenwiik"}}
-                  discord={{name: "Revolution#1234"}}
-                />
-              </Grid>
-              <Grid item container xs={12} sm={12} md={6} >
-                <PersonInfoCard
-                  avatar={"/assets/tko_avatar.jpg"}
-                  name="Tomas Klungerbo Olsen"
-                  github={{link: "https://github.com/Klungerbo", name: "Klungerbo"}}
-                  discord={{name: "Nopsa#6931"}}
-                />
-              </Grid>
-            </Grid>
+      <Grid container spacing={2} style={{ maxWidth: "max-content" }}>
+        <Grid item container xs={12} sm={12} md={6}>
+          <PersonInfoCard
+            avatar={"/assets/snw_avatar.jpg"}
+            name="Simen Nesse Wiik"
+            github={{ link: "https://github.com/revosw", name: "revosw" }}
+            soundcloud={{ link: "https://soundcloud.com/simenwiik", name: "simenwiik" }}
+            discord={{ name: "Revolution#1234" }}
+          />
+        </Grid>
+        <Grid item container xs={12} sm={12} md={6} >
+          <PersonInfoCard
+            avatar={"/assets/tko_avatar.jpg"}
+            name="Tomas Klungerbo Olsen"
+            github={{ link: "https://github.com/Klungerbo", name: "Klungerbo" }}
+            discord={{ name: "Nopsa#6931" }}
+          />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
